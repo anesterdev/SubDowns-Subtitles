a334fcc4a3a28fa82eb163c8a95d40e4db6f659f
Alex <alex@examle.com>
2026-06-20 18:16:02 +0300

    fix: `youtu.be` links parsing err

--- src/frontend/pages/Downloader.vue
+++ src/frontend/pages/Downloader.vue
@@ -1,21 +1,35 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
 import DownloaderHero from '../components/DownloaderHero.vue';
+import { toast } from 'vue3-toastify';
 
 const router = useRouter();
 
 function handleDownload(url: string) {
   try {
     const parsedUrl = new URL(url);
-    const vidId = parsedUrl.searchParams.get('v');
+    let vidId = '';
+    
+    if (parsedUrl.hostname === 'youtu.be') {
+      vidId = parsedUrl.pathname.slice(1);
+    } else {
+      vidId = parsedUrl.searchParams.get('v') || '';
+    }
+    
+    if (!vidId && parsedUrl.pathname.startsWith('/embed/')) {
+      vidId = parsedUrl.pathname.split('/')[2];
+    }
+    if (!vidId && parsedUrl.pathname.startsWith('/shorts/')) {
+      vidId = parsedUrl.pathname.split('/')[2];
+    }
     
     if (vidId) {
       router.push(`/preview?vid_id=${vidId}`);
     } else {
-      alert('Invalid YouTube URL. Could not extract video ID.');
+      toast.error('Invalid YouTube URL. Could not extract video ID.');
     }
   } catch (e) {
-    alert('Please enter a valid URL.');
+    toast.error('Please enter a valid URL.');
   }
 }
 </script>
